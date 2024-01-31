6d02201051d67ad4181b51a807267e1ddfe4ec01
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2024-01-31 13:59:10 -0300

    update1

--- Aplicação/script.js
+++ Aplicação/script.js
@@ -1,4 +1,15 @@
 const botaoPlayPause = document.getElementById('play-pause');
+const audiocapitulo = document.getElementById('audio-capitulo');
 
 const numeroCapitulos = 10;
 
+function tocarFaixa(){
+
+audiocapitulo.play();
+
+
+}
+
+botaoPlayPause.addEventListener(){
+    
+}
