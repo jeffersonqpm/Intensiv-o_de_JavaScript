7584a63b91f7ef58391a00a7ce71ce0ccc8148c2
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2024-01-31 15:12:42 -0300

    Colocando o botão play para funcionar

--- Aplicação/script.js
+++ Aplicação/script.js
@@ -2,14 +2,37 @@ const botaoPlayPause = document.getElementById('play-pause');
 const audiocapitulo = document.getElementById('audio-capitulo');
 
 const numeroCapitulos = 10;
+let taTocando = 0;
 
-function tocarFaixa(){
+function tocarFaixa() {
 
-audiocapitulo.play();
+    audiocapitulo.play();
+}
 
+function pausarFaixa() {
+
+    audiocapitulo.pause();
 
 }
 
-botaoPlayPause.addEventListener(){
-    
+botaoPlayPause.addEventListener('click', tocarOuPausar);
+// botaoPlayPause.onclick(tocarFaixa); // pode usar este tambem
+// função suprema
+function tocarOuPausar() {
+    if (taTocando === 0) {
+
+        tocarFaixa();
+        taTocando = 1;
+
+    } else {
+        pausarFaixa();
+        // botaoPlayPause.addEventListener('click', pausarFaixa);
+        taTocando = 0;
+    }
+
 }
+
+
+
+
+
